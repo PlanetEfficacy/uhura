e3cec746108de42288f4791642033deccb4d9172
PlanetEfficacy <jspevack@gmail.com>
2016-11-07 21:15:23 -0700

    refactor bulk message

--- spec/services/bulk_message_spec.rb
+++ spec/services/bulk_message_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe BulkMessage do
-  VCR.use_cassette("bulk_message_group_students") do
-    it "can send a message to a group of students" do
+  it "can send a message to a group of students" do
+    VCR.use_cassette("bulk_message_group_students") do
       user = FactoryGirl.create(:user, language_code: "en")
       group = FactoryGirl.create(:group, user: user)
       student_1 = FactoryGirl.create(:student, language_code: "en")
@@ -12,16 +12,22 @@ RSpec.describe BulkMessage do
       FactoryGirl.create(:enrollment, group: group, student: student_1)
       FactoryGirl.create(:enrollment, group: group, student: student_2)
 
-      FactoryGirl.create(:contact, contactable: student_1, phone_number: ENV["PHONE"])
-      FactoryGirl.create(:contact, contactable: student_2, phone_number: ENV["PHONE"])
+      contact_1 = FactoryGirl.create(:contact, contactable: student_1, phone_number: ENV["PHONE"])
+      contact_2 = FactoryGirl.create(:contact, contactable: student_2, phone_number: ENV["PHONE"])
 
       body = "This is a test text message"
 
       bulk_message = BulkMessage.new(group, body)
-      return_value = bulk_message.message_students
+      messages = bulk_message.message_students
 
-      expect(return_value.first.class).to eq(Message)
-      expect(return_value.last.class).to eq(Message)
+      expect(messages.first.class).to eq(Message)
+      expect(messages.first.user_id).to eq(user.id)
+      expect(messages.first.contact_id).to eq(contact_1.id)
+      expect(messages.first.body).to eq(body)
+      expect(messages.last.class).to eq(Message)
+      expect(messages.last.user_id).to eq(user.id)
+      expect(messages.last.contact_id).to eq(contact_2.id)
+      expect(messages.last.body).to eq(body)
     end
   end
 end

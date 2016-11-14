eee654f536ce6164b9d23e3c29546b54eacab27d
PlanetEfficacy <jspevack@gmail.com>
2016-11-14 12:01:00 -0700

    add before action to group guradians messages and group messages controllers

--- app/controllers/group_guardians_messages_controller.rb
+++ app/controllers/group_guardians_messages_controller.rb
@@ -2,12 +2,10 @@ class GroupGuardiansMessagesController < ApplicationController
   before_action :current_group
 
   def new
-    # @group = current_group
     @message = current_user.messages.new
   end
 
   def create
-    # @group = current_group
     bulk_message = BulkMessage.new(@group, message_params[:body])
     bulk_message.message_guardians
     redirect_to group_path(@group)
